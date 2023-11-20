3c710e98c45b17f238d26929edb9ca676dff3d97
Anibal Alvarez <aalvarez@chmw.co>
2023-11-20 19:02:29 -0400

    fix(discounts): Added code as unique identifier

--- src/Repositories/DiscountRepository.php
+++ src/Repositories/DiscountRepository.php
@@ -2,7 +2,25 @@
 
 namespace Repositories;
 
+use Doctrine\ORM\AbstractQuery;
+use Doctrine\ORM\NonUniqueResultException;
+use Entities\Entity;
+
 class DiscountRepository extends BaseRepository
 {
-
+    /**
+     * @param string $code
+     * @return array|null
+     * @throws NonUniqueResultException
+     */
+    public function getByCode(string $code): ?Entity
+    {
+        return $this->_em->createQueryBuilder()
+            ->select('e')
+            ->from($this->_entityName, 'e')
+            ->where('e.code = :code')
+            ->setParameter('code', $code)
+            ->getQuery()
+            ->getOneOrNullResult(AbstractQuery::HYDRATE_OBJECT);
+    }
 }
