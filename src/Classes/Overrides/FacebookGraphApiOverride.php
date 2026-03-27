c268035c6960216940f3611a621ab11abd9ef3d3
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-03-26 23:05:14 -0400

    Integrated Fix: Attempting to recover 'updated_time', 'admin_creator', and 'actions' for Facebook Posts in v25.0 stabilized override

--- src/Classes/Overrides/FacebookGraphApiOverride.php
+++ src/Classes/Overrides/FacebookGraphApiOverride.php
@@ -39,7 +39,7 @@ class FacebookGraphApiOverride extends FacebookGraphApi
         array $additionalParams = [],
     ): array {
         $query = [
-            'fields' => 'id,message,created_time,full_picture,permalink_url,shares,is_hidden,is_published,status_type,attachments,from,message_tags,story,story_tags',
+            'fields' => 'id,message,created_time,updated_time,full_picture,permalink_url,shares,is_hidden,is_published,status_type,attachments,from,message_tags,story,story_tags,admin_creator,actions,application,is_eligible_for_promotion,is_expired',
             'limit' => min($limit, 100),
         ];
 
