1a5f1f7b854ae0e20bbd2fa009afcc64d176f37c
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-03-26 22:24:46 -0400

    Application Fix: Overrode getFacebookPageInsights with correct signature and experimental v25.0 metric filter

--- src/Classes/Overrides/FacebookGraphApiOverride.php
+++ src/Classes/Overrides/FacebookGraphApiOverride.php
@@ -151,4 +151,37 @@ class FacebookGraphApiOverride extends FacebookGraphApi
             throw new Exception("Failed to retrieve media for Instagram ID ".$igUserId.": ".$e->getMessage());
         }
     }
+
+    /**
+     * @param string $pageId
+     * @param string|null $since
+     * @param string|null $until
+     * @param string|null $period
+     * @param string|array|null $metrics
+     * @param array $additionalParams
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getFacebookPageInsights(
+        string $pageId,
+        ?string $since = null,
+        ?string $until = null,
+        \Anibalealvarezs\FacebookGraphApi\Enums\MetricSet $metricSet = \Anibalealvarezs\FacebookGraphApi\Enums\MetricSet::BASIC,
+        array $customMetrics = [],
+    ): array {
+        // Experimental Purge for v25.0:
+        // If we are using the BASIC set, we intercept it and switch to a sanitized CUSTOM list
+        if ($metricSet === \Anibalealvarezs\FacebookGraphApi\Enums\MetricSet::BASIC && empty($customMetrics)) {
+            $customMetrics = ['page_impressions', 'page_impressions_paid', 'page_video_views'];
+            $metricSet = \Anibalealvarezs\FacebookGraphApi\Enums\MetricSet::CUSTOM;
+        }
+
+        return parent::getFacebookPageInsights(
+            pageId: $pageId,
+            since: $since,
+            until: $until,
+            metricSet: $metricSet,
+            customMetrics: $customMetrics
+        );
+    }
 }
