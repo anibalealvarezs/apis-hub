16ac264f8290da14ab7a3cfb308d9a0633234f45
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-04-12 21:59:58 -0400

    fix: improved routing resilience for trailing slashes

--- src/Classes/RoutingCore.php
+++ src/Classes/RoutingCore.php
@@ -50,7 +50,13 @@ class RoutingCore implements HttpKernelInterface
 
 
         try {
-            $trimmedPath = trim(urldecode($request->getPathInfo()), " \t\n\r\0\x0B");
+            $pathInfo = $request->getPathInfo();
+            $trimmedPath = ($pathInfo !== '/' && str_ends_with($pathInfo, '/')) 
+                ? substr($pathInfo, 0, -1) 
+                : $pathInfo;
+            
+            $trimmedPath = trim(urldecode($trimmedPath), " \t\n\r\0\x0B");
+            
             error_log("RoutingCore::handle - matching path: $trimmedPath");
             $attributes = $matcher->match($trimmedPath);
             error_log("RoutingCore::handle - matched route: " . ($attributes['_route'] ?? 'none'));
