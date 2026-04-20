0048cc0ef7e0860292c0d6b8e85ecda8da60e2d5
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-04-20 12:02:21 -0400

    fix: ensure platformId safety and optimize asset sync performance

--- src/Controllers/ConfigManagerController.php
+++ src/Controllers/ConfigManagerController.php
@@ -484,6 +484,11 @@ class ConfigManagerController extends BaseController
                         } else {
                             $platformId = $rawPlatformId;
                         }
+
+                        if (empty($platformId)) {
+                            continue;
+                        }
+
                         $platformCreatedAt = $asset[$pattern['channeled_account']['platform_created_at_key']] ?? null;
                         $typeMark = $pattern['channeled_account']['type'];
                         $name = $asset[$pattern['channeled_account']['name_key']] ?? null;
