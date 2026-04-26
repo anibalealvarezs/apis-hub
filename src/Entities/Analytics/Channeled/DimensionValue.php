f0acee8374f4d81ebe94204eb9670151cbabe131
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-04-25 23:41:20 -0400

    Normalize DimensionValue formatting after length update

--- src/Entities/Analytics/Channeled/DimensionValue.php
+++ src/Entities/Analytics/Channeled/DimensionValue.php
@@ -1,43 +1,45 @@
 <?php
 
-namespace Entities\Analytics\Channeled;
+    namespace Entities\Analytics\Channeled;
 
-use Doctrine\ORM\Mapping as ORM;
-use Entities\Entity;
-use Repositories\Channeled\DimensionValueRepository;
-use Entities\Analytics\Channeled\DimensionKey;
+    use Doctrine\ORM\Mapping as ORM;
+    use Entities\Entity;
+    use Repositories\Channeled\DimensionValueRepository;
+    use Entities\Analytics\Channeled\DimensionKey;
 
-#[ORM\Entity(repositoryClass: DimensionValueRepository::class)]
-#[ORM\Table(name: 'dimension_values')]
-#[ORM\UniqueConstraint(name: 'dimension_value_unique', columns: ['dimension_key_id', 'value'])]
-class DimensionValue extends Entity
-{
-    #[ORM\ManyToOne(targetEntity: DimensionKey::class, inversedBy: 'values', cascade: ['persist'])]
-    #[ORM\JoinColumn(name: 'dimension_key_id', nullable: false, onDelete: 'CASCADE')]
-    protected DimensionKey $dimensionKey;
-
-    #[ORM\Column(type: 'string', length: 512)]
-    protected string $value;
-
-    public function getDimensionKey(): DimensionKey
+    #[ORM\Entity(repositoryClass: DimensionValueRepository::class)]
+    #[ORM\Table(name: 'dimension_values')]
+    #[ORM\UniqueConstraint(name: 'dimension_value_unique', columns: ['dimension_key_id', 'value'])]
+    class DimensionValue extends Entity
     {
-        return $this->dimensionKey;
-    }
+        #[ORM\ManyToOne(targetEntity: DimensionKey::class, cascade: ['persist'], inversedBy: 'values')]
+        #[ORM\JoinColumn(name: 'dimension_key_id', nullable: false, onDelete: 'CASCADE')]
+        protected DimensionKey $dimensionKey;
 
-    public function setDimensionKey(DimensionKey $dimensionKey): self
-    {
-        $this->dimensionKey = $dimensionKey;
-        return $this;
-    }
+        #[ORM\Column(type: 'string', length: 512)]
+        protected string $value;
 
-    public function getValue(): string
-    {
-        return $this->value;
-    }
+        public function getDimensionKey(): DimensionKey
+        {
+            return $this->dimensionKey;
+        }
 
-    public function setValue(string $value): self
-    {
-        $this->value = $value;
-        return $this;
+        public function setDimensionKey(DimensionKey $dimensionKey): self
+        {
+            $this->dimensionKey = $dimensionKey;
+
+            return $this;
+        }
+
+        public function getValue(): string
+        {
+            return $this->value;
+        }
+
+        public function setValue(string $value): self
+        {
+            $this->value = $value;
+
+            return $this;
+        }
     }
-}
