0970987e797481a353378d02635f29eeb2d8f56e
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-05-06 10:12:35 -0400

    updated script

--- bin/dump-failed-jobs.php
+++ bin/dump-failed-jobs.php
@@ -13,11 +13,12 @@ $failedJobs = $jobRepo->findBy(
     5
 );
 
-echo "Last 5 Failed Jobs:\n";
-echo "-------------------\n";
+echo "Last 5 Failed Jobs (with Timestamps):\n";
+echo "------------------------------------\n";
 foreach ($failedJobs as $job) {
     echo "ID: " . $job->getUuid() . "\n";
+    echo "Time: " . $job->getUpdatedAt()->format('Y-m-d H:i:s') . "\n";
     echo "Channel: " . $job->getChannel() . "\n";
     echo "Message: " . $job->getMessage() . "\n";
-    echo "-------------------\n";
+    echo "------------------------------------\n";
 }
