7aa05b2aa3d5b440571bdd6de741d736d99709fd
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-05-05 20:29:12 -0400

    fixed aggregation telemetry

--- src/Commands/Analytics/ReportAggregationTelemetryCommand.php
+++ src/Commands/Analytics/ReportAggregationTelemetryCommand.php
@@ -4,7 +4,7 @@
 
     namespace Commands\Analytics;
 
-    use Traits\AggregationFallbackTelemetryReporter;
+    use Services\Aggregation\AggregationFallbackTelemetryReporter;
     use Symfony\Component\Console\Attribute\AsCommand;
     use Symfony\Component\Console\Command\Command;
     use Symfony\Component\Console\Input\InputInterface;
