e89f526bcef8fbf0da1bdbfda924c16c6f1b889e
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-05-16 11:14:26 -0400

    Invalidate sync telemetry command refactor

--- src/Commands/Analytics/ClearCacheCommand.php
+++ src/Commands/Analytics/ClearCacheCommand.php
@@ -1,96 +1,110 @@
 <?php
 
-declare(strict_types=1);
+    declare(strict_types=1);
 
-namespace Commands\Analytics;
+    namespace Commands\Analytics;
 
-use Helpers\Helpers;
-use Services\CacheService;
-use Symfony\Component\Console\Attribute\AsCommand;
-#[AsCommand(
-    name: 'cache:clear',
-    description: 'Clears the Redis cache (all, by channel, or by entity).',
-    aliases: ['app:cache:clear'],
-    hidden: false
-)]
-class ClearCacheCommand extends \Symfony\Component\Console\Command\Command
-{
-    private ?CacheService $cacheService;
+    use Exceptions\ConfigurationException;
+    use Helpers\Helpers;
+    use Services\CacheService;
+    use Symfony\Component\Console\Attribute\AsCommand;
+    use Symfony\Component\Console\Command\Command;
+    use Symfony\Component\Console\Input\InputInterface;
+    use Symfony\Component\Console\Input\InputOption;
+    use Symfony\Component\Console\Output\OutputInterface;
 
-    public function __construct(?CacheService $cacheService = null)
+    #[AsCommand(
+        name: 'cache:clear',
+        description: 'Clears the Redis cache (all, by channel, or by entity).',
+        aliases: ['app:cache:clear'],
+        hidden: false
+    )]
+    class ClearCacheCommand extends Command
     {
-        $this->cacheService = $cacheService;
-        parent::__construct();
+        private ?CacheService $cacheService;
+
+        public function __construct(?CacheService $cacheService = null)
+        {
+            $this->cacheService = $cacheService;
+            parent::__construct();
+        }
+
+        protected function configure(): void
+        {
+            $this
+                ->addOption('channel', 'c', InputOption::VALUE_OPTIONAL, 'Clear cache for a specific channel')
+                ->addOption('entity', 'e', InputOption::VALUE_OPTIONAL, 'Clear cache for a specific entity')
+                ->addOption('all', 'a', InputOption::VALUE_NONE, 'Clear all cache');
+        }
+
+        /**
+         * @throws ConfigurationException
+         */
+        protected function execute(InputInterface $input, OutputInterface $output): int
+        {
+            $channel = $input->getOption('channel');
+            $entity = $input->getOption('entity');
+            $all = $input->getOption('all');
+
+            $cache = $this->cacheService ?? CacheService::getInstance(Helpers::getRedisClient());
+
+            if ($all) {
+                if (Helpers::isDebug()) {
+                    $output->writeln('<comment>Clearing all cache...</comment>');
+                }
+                $cache->deletePattern('*');
+                if (Helpers::isDebug()) {
+                    $output->writeln('<info>All cache cleared.</info>');
+                }
+
+                return self::SUCCESS;
+            }
+
+            if ($channel && $entity) {
+                if (Helpers::isDebug()) {
+                    $output->writeln("<comment>Clearing cache for channel '$channel' and entity '$entity'...</comment>");
+                }
+                // List and Count patterns for channeled entities
+                $cache->deletePattern("channeled_list_{$entity}_{$channel}_*");
+                $cache->deletePattern("channeled_count_{$entity}_{$channel}_*");
+                // Standard ones just in case
+                $cache->deletePattern("list_{$entity}_*");
+                $cache->deletePattern("count_{$entity}_*");
+                if (Helpers::isDebug()) {
+                    $output->writeln('<info>Filtered cache cleared.</info>');
+                }
+
+                return self::SUCCESS;
+            }
+
+            if ($channel) {
+                if (Helpers::isDebug()) {
+                    $output->writeln("<comment>Clearing cache for channel '$channel'...</comment>");
+                }
+                $cache->deletePattern("*_{$channel}_*");
+                if (Helpers::isDebug()) {
+                    $output->writeln('<info>Channel cache cleared.</info>');
+                }
+
+                return self::SUCCESS;
+            }
+
+            if ($entity) {
+                if (Helpers::isDebug()) {
+                    $output->writeln("<comment>Clearing cache for entity '$entity'...</comment>");
+                }
+                $cache->deletePattern("*_{$entity}_*");
+                $cache->deletePattern("list_{$entity}_*");
+                $cache->deletePattern("count_{$entity}_*");
+                if (Helpers::isDebug()) {
+                    $output->writeln('<info>Entity cache cleared.</info>');
+                }
+
+                return self::SUCCESS;
+            }
+
+            $output->writeln('<error>Please specify --all, --channel, or --entity.</error>');
+
+            return self::FAILURE;
+        }
     }
-
-    protected function configure(): void
-    {
-        $this
-            ->addOption('channel', 'c', \Symfony\Component\Console\Input\InputOption::VALUE_OPTIONAL, 'Clear cache for a specific channel')
-            ->addOption('entity', 'e', \Symfony\Component\Console\Input\InputOption::VALUE_OPTIONAL, 'Clear cache for a specific entity')
-            ->addOption('all', 'a', \Symfony\Component\Console\Input\InputOption::VALUE_NONE, 'Clear all cache');
-    }
-
-    protected function execute(\Symfony\Component\Console\Input\InputInterface $input, \Symfony\Component\Console\Output\OutputInterface $output): int
-    {
-        $channel = $input->getOption('channel');
-        $entity = $input->getOption('entity');
-        $all = $input->getOption('all');
-
-        $cache = $this->cacheService ?? CacheService::getInstance(Helpers::getRedisClient());
-
-        if ($all) {
-            if (Helpers::isDebug()) {
-                $output->writeln('<comment>Clearing all cache...</comment>');
-            }
-            $cache->deletePattern('*');
-            if (Helpers::isDebug()) {
-                $output->writeln('<info>All cache cleared.</info>');
-            }
-            return self::SUCCESS;
-        }
-
-        if ($channel && $entity) {
-            if (Helpers::isDebug()) {
-                $output->writeln("<comment>Clearing cache for channel '$channel' and entity '$entity'...</comment>");
-            }
-            // List and Count patterns for channeled entities
-            $cache->deletePattern("channeled_list_{$entity}_{$channel}_*");
-            $cache->deletePattern("channeled_count_{$entity}_{$channel}_*");
-            // Standard ones just in case
-            $cache->deletePattern("list_{$entity}_*");
-            $cache->deletePattern("count_{$entity}_*");
-            if (Helpers::isDebug()) {
-                $output->writeln('<info>Filtered cache cleared.</info>');
-            }
-            return self::SUCCESS;
-        }
-
-        if ($channel) {
-            if (Helpers::isDebug()) {
-                $output->writeln("<comment>Clearing cache for channel '$channel'...</comment>");
-            }
-            $cache->deletePattern("*_{$channel}_*");
-            if (Helpers::isDebug()) {
-                $output->writeln('<info>Channel cache cleared.</info>');
-            }
-            return self::SUCCESS;
-        }
-
-        if ($entity) {
-            if (Helpers::isDebug()) {
-                $output->writeln("<comment>Clearing cache for entity '$entity'...</comment>");
-            }
-            $cache->deletePattern("*_{$entity}_*");
-            $cache->deletePattern("list_{$entity}_*");
-            $cache->deletePattern("count_{$entity}_*");
-            if (Helpers::isDebug()) {
-                $output->writeln('<info>Entity cache cleared.</info>');
-            }
-            return self::SUCCESS;
-        }
-
-        $output->writeln('<error>Please specify --all, --channel, or --entity.</error>');
-        return self::FAILURE;
-    }
-}
--- src/Commands/Analytics/InvalidateSyncCacheCommand.php
+++ src/Commands/Analytics/InvalidateSyncCacheCommand.php
@@ -5,19 +5,31 @@
     use Helpers\Helpers;
     use Services\Sync\SyncTelemetryService;
     use Services\CacheService;
+    use Symfony\Component\Console\Attribute\AsCommand;
     use Symfony\Component\Console\Command\Command;
     use Symfony\Component\Console\Input\InputInterface;
     use Symfony\Component\Console\Input\InputOption;
     use Symfony\Component\Console\Output\OutputInterface;
 
+    #[AsCommand(
+        name: 'cache:invalidate-sync',
+        description: 'Invalidates the sync telemetry cache.',
+        aliases: ['app:cache:invalidate-sync'],
+        hidden: false
+    )]
     class InvalidateSyncCacheCommand extends Command
     {
-        protected static string $defaultName = 'cache:invalidate-sync';
+        private SyncTelemetryService $telemetryService;
+
+        public function __construct(?SyncTelemetryService $telemetryService = null)
+        {
+            $this->telemetryService = $telemetryService ?? new SyncTelemetryService(new CacheService(Helpers::getRedisClient()));
+            parent::__construct();
+        }
 
         protected function configure(): void
         {
             $this
-                ->setDescription('Invalidates the sync telemetry cache.')
                 ->addOption('channel', 'c', InputOption::VALUE_OPTIONAL, 'The channel to invalidate.')
                 ->addOption('account', 'a', InputOption::VALUE_OPTIONAL, 'The account ID to invalidate (requires --channel).');
         }
@@ -27,11 +39,7 @@
             $channel = $input->getOption('channel');
             $account = $input->getOption('account');
 
-            $redisClient = Helpers::getRedisClient();
-            $cacheService = new CacheService($redisClient);
-            $telemetryService = new SyncTelemetryService($cacheService);
-
-            $telemetryService->invalidate($channel, $account);
+            $this->telemetryService->invalidate($channel, $account);
 
             if ($channel && $account) {
                 $output->writeln("<info>✅ Invalidated cache for account '$account' in channel '$channel'.</info>");
