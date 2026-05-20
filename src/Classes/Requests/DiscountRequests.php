79009e4c9e0c5af4b7dd55f95fed9749bead4acf
Aníbal Álvarez <anibalealvarezs@gmail.com>
2026-05-20 17:48:11 -0400

    Add static setSyncService method

--- src/Classes/Requests/DiscountRequests.php
+++ src/Classes/Requests/DiscountRequests.php
@@ -1,53 +1,66 @@
 <?php
 
-declare(strict_types=1);
+    declare(strict_types=1);
 
-namespace Classes\Requests;
+    namespace Classes\Requests;
 
-use Doctrine\Common\Collections\ArrayCollection;
-use Entities\Analytics\Channel;
-use Interfaces\RequestInterface;
-use Psr\Log\LoggerInterface;
-use Symfony\Component\HttpFoundation\Response;
+    use Core\Services\SyncService;
+    use Doctrine\Common\Collections\ArrayCollection;
+    use Entities\Analytics\Channel;
+    use Interfaces\RequestInterface;
+    use Psr\Log\LoggerInterface;
+    use Symfony\Component\HttpFoundation\Response;
+    use Throwable;
 
-class DiscountRequests implements RequestInterface
-{
-    /**
-     * @param Channel|string $channel
-     * @param string|null $startDate
-     * @param string|null $endDate
-     * @param \Psr\Log\LoggerInterface|null $logger
-     * @param int|null $jobId
-     * @param object|null $filters
-     * @return \Symfony\Component\HttpFoundation\Response
-     * @throws \Exception
-     */
-    public static function getList(
-        object|string $channel,
-        ?string $startDate = null,
-        ?string $endDate = null,
-        ?LoggerInterface $logger = null,
-        ?int $jobId = null,
-        ?object $filters = null
-    ): Response {
-        $chanKey = is_object($channel) ? ($channel->name ?? (method_exists($channel, 'getName') ? $channel->getName() : (string)$channel)) : (string)$channel;
-
-        return (new \Core\Services\SyncService())->execute($chanKey, $startDate, $endDate, [
-            'jobId' => $jobId,
-            'resume' => $filters->resume ?? true,
-            'type' => 'discounts',
-            'filters' => $filters,
-        ]);
-    }
-
-    /**
-     * @param ArrayCollection $channeledCollection
-     * @return Response
-     */
-    public static function process(ArrayCollection $channeledCollection, ?LoggerInterface $logger = null): Response
+    class DiscountRequests implements RequestInterface
     {
-        // TODO: Implement process() method.
+        private static ?SyncService $syncService = null;
 
-        return new Response(json_encode(['Discounts processed']));
+        public static function setSyncService(?SyncService $syncService): void
+        {
+            self::$syncService = $syncService;
+        }
+
+        /**
+         * @param object|string $channel
+         * @param string|null $startDate
+         * @param string|null $endDate
+         * @param LoggerInterface|null $logger
+         * @param int|null $jobId
+         * @param object|null $filters
+         * @return Response
+         * @throws Throwable
+         */
+        public static function getList(
+            object|string    $channel,
+            ?string          $startDate = null,
+            ?string          $endDate = null,
+            ?LoggerInterface $logger = null,
+            ?int             $jobId = null,
+            ?object          $filters = null
+        ): Response
+        {
+            $chanKey = is_object($channel) ? ($channel->name ?? (method_exists($channel, 'getName') ? $channel->getName() : (string)$channel)) : (string)$channel;
+
+            $syncService = self::$syncService ?? new SyncService();
+
+            return $syncService->execute($chanKey, $startDate, $endDate, [
+                'jobId'   => $jobId,
+                'resume'  => $filters->resume ?? true,
+                'type'    => 'discounts',
+                'filters' => $filters,
+            ]);
+        }
+
+        /**
+         * @param ArrayCollection $channeledCollection
+         * @param LoggerInterface|null $logger
+         * @return Response
+         */
+        public static function process(ArrayCollection $channeledCollection, ?LoggerInterface $logger = null): Response
+        {
+            // TODO: Implement process() method.
+
+            return new Response(json_encode(['Discounts processed']));
+        }
     }
-}
